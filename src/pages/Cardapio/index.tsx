a5c1da39d416654e2ae38b496c0cfadeaca538d9
thallysbraz <thallysbraz3@gmail.com>
2022-09-22 23:44:08 -0300

    iniciando projeto

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Cardapio.module.scss";
+
+export default function Cardapio() {
+	return (
+		<main>
+			<nav className="menu"></nav>
+		</main>
+	);
+}
